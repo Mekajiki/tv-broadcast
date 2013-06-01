27ea8478b5585f504fe4b6bda52cada85308b77b
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2013-06-01 09:57:33 +0000

    Add FacebookAuthenticator

---
+++ app/models/facebook_authenticator.rb
@@ -0,0 +1,32 @@
+class FacebookAuthenticator
+  def initialize(auth)
+    @auth = auth
+  end
+
+  def create_or_update_user
+    user = User.where(uid: uid).first || User.new
+    user.update_attributes(uid: uid, name: name, token: token)
+    user
+  end
+
+  private
+    def uid
+      @uid ||= @auth.uid
+    end
+
+    def info
+      @info ||= @auth.info
+    end
+
+    def credentials
+      @credentials ||= @auth.credentials
+    end
+
+    def name
+      @name ||= info.name
+    end
+
+    def token
+      @token ||= credentials.token
+    end
+end
