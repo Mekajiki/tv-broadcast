afa4b85f0de9050c9dd3de9be13107a012dac8f5
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2013-06-05 00:33:41 +0000

    Add User#pending? etc.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ActiveRecord::Base
   scope :pending, -> { where(status: :pending) }
   scope :active, -> { where(status: :active) }
   scope :suspended, -> { where(status: :suspended) }
+
+  def pending?
+    status == 'pending'
+  end
+
+  def active?
+    status == 'active'
+  end
+
+  def suspended?
+    status == 'suspended'
+  end
 end
