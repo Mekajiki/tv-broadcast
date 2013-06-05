707e9b238a7ebd8ce0031b31400f0276da6ace5d
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2013-06-05 00:31:30 +0000

    Add User.pending etc.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   validates :uid, presence: true, uniqueness: true
+
+  scope :pending, -> { where(status: :pending) }
+  scope :active, -> { where(status: :active) }
+  scope :suspended, -> { where(status: :suspended) }
 end
