75d452450e950b4f2b0454584d58e6a4425636b4
Yuichiro NAKAGAWA <ii.hsif.drows@gmail.com>
2013-08-08 06:14:26 +0900

    implements method to create gif from program

--- lib/movie/gif.rb
+++ lib/movie/gif.rb
@@ -9,5 +9,15 @@ module Movie::Gif
       System.exec "ffmpeg -ss #{start_at} -i #{source} -r 10 -t #{end_at - start_at} -s #{Settings.gif.resolution} #{prefix}%04d.gif"
       System.exec "convert -delay 10 -loop 1 #{prefix}*.gif #{out}"
     end
+
+    def convert_program(program, start_at, end_at)
+      source = program.movie_absolute_path
+      file_name = SecureRandom.hex + '.gif'
+      out_path = Settings.gif.storage + '/' + file_name
+
+      create(source, out_path, start_at, end_at)
+
+      return GifAnimation.create(program: program, start_at: start_at, end_at: end_at, file_name: file_name)
+    end
   end
 end
