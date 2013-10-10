00824ee9f89f9573f37395dcece801b50e116b15
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2013-10-10 16:54:51 +0900

    add progress bar on the top of the page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
+//= require nprogress
+//= require nprogress-turbolinks
 //= require keymaster
 //= require keymaster.sequence
 //= require_tree .
